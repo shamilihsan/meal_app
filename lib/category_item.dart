45d4562aba7120b66f06b56429dfd6ae8fd824c1
Shamil Ihsan <shamil.ihsan@hotmail.com>
2020-04-16 17:03:29 +0530

    Used named routes

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -2,17 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:meal_app/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(builder: (_) {
-        return CategoryMealsScreen();
-      }),
-    );
+    Navigator.of(context)
+        .pushNamed('/category-meals', arguments: {'id': id, 'title': title});
   }
 
   @override
